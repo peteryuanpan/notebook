f0e086e8f4481ad38e3edc14197b049435b8aff7
peteryuanpan <peteryuanpan@gmail.com>
2019-08-13 21:20:58 +0800

    20190813

---
+++ tree.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#File: tree-md
+
+tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
+	sed -e 's/| / /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' |
+	grep -v -x '.' | grep -v 'README.md' | grep -v '         ')
+
+printf "# Project tree\n\n${tree}"
