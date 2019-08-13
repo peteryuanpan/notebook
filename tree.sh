37f048028d4db6ecdc5b12b5ccbd885dd8df925b
peteryuanpan <peteryuanpan@gmail.com>
2019-08-13 21:27:44 +0800

    20190813

--- tree.sh
+++ tree.sh
@@ -2,7 +2,7 @@
 
 #File: tree-md
 
-tree=$(tree -tf --noreport -I '*~' --charset ascii $1 |
+tree=$(tree -f --noreport -I '*~' --charset ascii $1 |
 	sed -e 's/| / /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' |
 	grep -v -x '.' | grep -v 'README.md' | grep -v '         ')
 
