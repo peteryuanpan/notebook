187dbc72764273176d5d7ff2875c33aab76d6f96
Peter <peteryuanpan@gmail.com>
2019-12-25 16:25:27 +0800

    Update solve.cpp

--- ALGORITHM_DATASTRUCTURE/ICPC/白题/leetcode_problem15_求和为零的三元组的去重解/solve.cpp
+++ ALGORITHM_DATASTRUCTURE/ICPC/白题/leetcode_problem15_求和为零的三元组的去重解/solve.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
-	vector<vector<int>> threeSum(vector<int>& nums) {
-		vector<vector<int>> ans;
-		if(nums.size() <= 2) return ans;
-		
-		sort(nums.begin(),nums.end());
-		
-		for(int i = 0 ; i < nums.size();) {
-			int left = i+1;
-			int right = nums.size()-1;
-			while(right > left) {
-				if(nums[right] + nums[left] + nums[i] == 0) {
-					vector<int> t;
-					t.push_back(nums[right]);
-					t.push_back(nums[left]);
-					t.push_back(nums[i]);
-					ans.push_back(t);
-					
-					//skip over duplicates		
-					while(right > left && nums[right] == nums[right-1]) {
-						right--;
-					}
-					while(right > left && nums[left] == nums[left+1]) { 
-						left++;
-					}
-					
-					right--;
-					left++;
-					
-				} else if(nums[right] + nums[left] + nums[i] > 0) {
-					right--;
-				} else if (nums[right] + nums[left] + nums[i] < 0) {
-					left++;
-				}
-			}
-			
-			//skip over duplicates
-			while(i+1 < nums.size() && nums[i] == nums[i+1]) {
-				i++;
-			}
-			i++;
-		}
-		
-		return ans;
-	}
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < nums.size(); i ++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            
+            for (int j = i + 1; j < nums.size(); j ++) {
+                if (j > i + 1 && nums[j] == nums[j-1]) continue;
+                
+                int l = j + 1;
+                int r = nums.size() - 1;
+                while (l < r) {
+                    int mid = (l + r) / 2;
+                    int sum = nums[i] + nums[j] + nums[mid];
+                    if (sum == 0) {
+                        l = r = mid;
+                        break;
+                    }
+                    if (sum < 0) l = mid + 1;
+                    else r = mid - 1;
+                }
+                
+                if (l == r && l >= 0 && l < nums.size()) {
+                    if (nums[i] + nums[j] + nums[l] == 0) {
+                        vector<int> t;
+                        t.push_back(nums[i]);
+                        t.push_back(nums[j]);
+                        t.push_back(nums[l]);
+                        ans.push_back(t);
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
 };
