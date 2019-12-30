f838e4b00a61984c6397051302b8b96ddbdba39b
peteryuanpan <peteryuanpan@gmail.com>
2019-12-30 11:34:43 +0800

    20191230

---
+++ tree.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tree=$(tree --dirsfirst -f --noreport -I '*~' --charset ascii $1 |
+	sed -e 's/| / /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' |
+	grep -v -x '.' | grep -v 'README.md' | grep -v '.png' | grep -v '         ')
+
+printf "# Notebook\n\n${tree}" | sed "s/.md]/]/g"
