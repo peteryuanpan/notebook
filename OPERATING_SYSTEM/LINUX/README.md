4b3f53368b8488193622e9edaed27249c30ee310
peteryuanpan <peteryuanpan@gmail.com>
2019-12-30 19:19:47 +0800

    20191230 重构目录表

--- OPERATING_SYSTEM/LINUX/README.md
+++
@@ -1,5 +0,0 @@
-# LINUX
-
-Linus Torvalds
-
-https://github.com/torvalds/linux
--- tree.sh
+++ tree.sh
@@ -2,6 +2,6 @@
 
 tree=$(tree --dirsfirst -f --noreport -I '*~' --charset ascii $1 |
 	sed -e 's/| / /g' -e 's/[|`]-\+/ */g' -e 's:\(* \)\(\(.*/\)\([^/]\+\)\):\1[\4](\2):g' |
-	grep -v -x '.' | grep -v 'README.md' | grep -v '.png' | grep -v '         ')
+	grep -v -x '.' | grep -v 'README.md' | grep -v '.png' | grep -v '     ')
 
 printf "# Notebook\n\n${tree}" | sed "s/.md]/]/g"
