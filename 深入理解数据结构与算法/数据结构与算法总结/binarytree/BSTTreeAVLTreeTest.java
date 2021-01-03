2033c7e3a9a923d51e26fd518a7c933fb46c2dd3
Peter <peteryuanpan@sina.com>
2021-01-03 22:47:21 +0800

    Update and rename AVLTreeTest.java to BSTTreeAVLTreeTest.java

--- 深入理解数据结构与算法/数据结构与算法总结/binarytree/BSTTreeAVLTreeTest.java
+++ 深入理解数据结构与算法/数据结构与算法总结/binarytree/BSTTreeAVLTreeTest.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class AVLTreeTest {
+public class BSTTreeAVLTreeTest {
 
     static BinarySearchTree<Integer, Integer> BSTTree = new BSTTree<>();
     static BinarySearchTree<Integer, Integer> AVLTree = new AVLTree<>();
