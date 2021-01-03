5ef8742603602d36f10bb8bbe97082989cf12886
Peter <peteryuanpan@sina.com>
2021-01-03 22:48:07 +0800

    Create BinarySearchTree.java

---
+++ 深入理解数据结构与算法/数据结构与算法总结/binarytree/BinarySearchTree.java
@@ -0,0 +1,97 @@
+package binarytree;
+
+import java.util.List;
+
+public interface BinarySearchTree<K, V> {
+
+    /**
+     * Get value if key exists or return null
+     * @param key
+     * @return
+     */
+    V get(K key);
+
+    /**
+     * If key not exists, put value and return<br/>
+     * If key exists, overwrite old value and return new value<br/>
+     * It is not allowed to put null key
+     * @param key
+     * @param value
+     * @return
+     */
+    V put(K key, V value);
+
+    /**
+     * Put only if key is absent
+     * @param key
+     * @param value
+     * @return
+     */
+    V putIfAbsent(K key, V value);
+
+    /**
+     * Put key, key
+     * @param key
+     * @return V
+     */
+    default V put(K key) {
+        return put(key, (V) key);
+    }
+
+    /**
+     * Put key, key only if key is absent
+     * @param key
+     * @return
+     */
+    default V putIfAbsent(K key) {
+        return putIfAbsent(key, (V) key);
+    }
+
+    /**
+     * Remove key and return value if key exists or return null
+     * @param key
+     * @return
+     */
+    V remove(K key);
+
+    /**
+     * Clear all nodes, size -> 0
+     */
+    void clear();
+
+    /**
+     * Return size of nodes
+     * @return
+     */
+    int size();
+
+    /**
+     * Return list of Entry K, V in Pre-order
+     * @return
+     */
+    List<Entry<K, V>> entryList();
+
+    /**
+     * Entry K, V
+     * @param <K>
+     * @param <V>
+     */
+    class Entry<K, V> {
+
+        private final K key;
+        private final V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+}
